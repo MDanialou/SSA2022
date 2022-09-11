02a1aabb83ef73ba9fed6e0da5db9d0848bf953e
meadows100 <93254561+meadows100@users.noreply.github.com>
2022-09-11 19:22:38 +0100

    Add files via upload

---
+++ encrypted_smart_meter.py
@@ -0,0 +1,22 @@
+import paho.mqtt.client as mqtt
+from random import randrange, uniform
+import time
+from cryptography.fernet import Fernet
+
+client = mqtt.Client("smart_meter")
+#client.username_pw_set("username", "password")
+client.connect("broker.emqx.io", 1883)
+
+#Create cipher and key to use for secure transmission
+cipher_key = b'70JZaJg4c5F7RIOhrSXNjq0Y0iGp1QtBy2gyVMSdHHY='
+cipher = Fernet(cipher_key)
+sum_of_units = 0
+
+while True:
+    sum_of_units = sum_of_units + randrange(10)
+    message = str(sum_of_units)
+    encrypted_message = cipher.encrypt(message.encode())
+    out_message = encrypted_message.decode()
+    client.publish("UNITS", out_message)
+    print("Published " + out_message + " to Topic UNITS")
+    time.sleep(2)
