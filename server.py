90837eed4c84c870d64d86091b3b8d2d544c3343
meadows100 <93254561+meadows100@users.noreply.github.com>
2022-09-17 22:31:23 +0100

    Improved code

--- server.py
+++ server.py
@@ -1,3 +1,6 @@
+"""Server script receives total energy units used
+from the smart meter via the public broker
+"""
 import time
 import paho.mqtt.client as mqtt
 from cryptography.fernet import Fernet
@@ -17,7 +20,7 @@ def on_message(client, userdata, message):
     """On receiving a message:  decrypt it, convert to an integer
     display the number of units used and current cost (to 4 x decimal points)
     kick off the publish function."""
-    decrypted_message = cipher.decrypt(message.payload)
+    decrypted_message = CIPHER.decrypt(message.payload)
     msg = int(decrypted_message.decode("utf-8"))
     print("Total Units = ", str(decrypted_message.decode("utf-8")))
     rounded = msg * 0.00039
@@ -29,7 +32,7 @@ def pub(client, topic, msg, qos):
     """Confirm total back to smart meter, encrypted via TOPIC2
     (used to prevent unit total resetting to 0)."""
     message = str(msg)
-    encrypted_message = cipher.encrypt(message.encode())
+    encrypted_message = CIPHER.encrypt(message.encode())
     out_message = encrypted_message.decode()
     client.publish(topic, out_message, qos)
     time.sleep(4)
@@ -40,7 +43,7 @@ def sub(client, topic, qos):
     client.subscribe(topic, qos)
 
 
-#Set Constants for server
+# Set Constants for server
 QOSS = 1
 BROKER = "broker.emqx.io"
 TOPIC1 = "UNITS1221"
@@ -49,7 +52,7 @@ PORT = 1883
 CIPHER_KEY = b'70JZaJg4c5F7RIOhrSXNjq0Y0iGp1QtBy2gyVMSdHHY='
 CIPHER = Fernet(CIPHER_KEY)
 
-inp = input("Welcome to the SSA prototype:\n Press Enter to connect to the broker")
+inp = input("Welcome to the SSA prototype:\n Press ENTER to connect to broker")
 
 # Setup client, connect to broker, and register callbacks to functions
 client = mqtt.Client("ServerSSA")
@@ -62,6 +65,6 @@ client.loop_start()
 time.sleep(2)
 
 # Give the user a way to end the program
-inp = input("\nWaiting to continue.  Press RETURN at any time to end program)\n")
+inp = input("\nWaiting to continue.  Press ENTER any time to end program)\n")
 print("Ending")
 client.loop_stop()
--- smart_meter.py
+++ smart_meter.py
@@ -1,3 +1,6 @@
+"""Smart_meter script generates simulated energy usage and appends to total
+units integer, then transmits this to the server via the public broker
+"""
 import time
 from random import randrange
 import paho.mqtt.client as mqtt
@@ -19,9 +22,9 @@ def on_message(client, userdata, message):
     Decrypt message, convert to integer, display to user the units and
     set msgflag to True for handling running total (without resetting to 0)
     then kick off publish function"""
-    decrypted_message = cipher.decrypt(message.payload)
+    decrypted_message = CIPHER.decrypt(message.payload)
     msg = int(decrypted_message.decode("utf-8"))
-    print("Unit Total from Server = ", str(decrypted_message.decode("utf-8")))
+    print("Unit total from server = ", str(decrypted_message.decode("utf-8")))
     pub(client, TOPIC1, msg, QOSS)
 
 
@@ -34,13 +37,13 @@ def pub(client, topic, msg, qos):
     """Publish encrypted unit total (simulated by adding a random numer)"""
     msg = msg + randrange(10)
     message = str(msg)
-    encrypted_message = cipher.encrypt(message.encode())
+    encrypted_message = CIPHER.encrypt(message.encode())
     out_message = encrypted_message.decode()
     client.publish(topic, out_message, qos, True)
     time.sleep(4)
 
 
-#Set Constants for smart meter
+# Set Constants for smart meter
 QOSS = 1
 BROKER = 'broker.emqx.io'
 TOPIC1 = "UNITS1221"
@@ -55,7 +58,7 @@ client.connect(BROKER, PORT)
 client.on_connect = on_connect
 client.on_message = on_message
 
-#Check message buffers
+# Check message buffers
 while True:
     client.loop_start()
     time.sleep(2)
